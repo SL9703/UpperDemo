5b289e01d62c00d301de5484688a3a379f408765
齐天大圣孙悟空 <3278341279@qq.com>
2023-03-18 20:05:31 +0800

    定时器功能

--- UpperComputerDemo1/Form1.Designer.cs
+++ UpperComputerDemo1/Form1.Designer.cs
@@ -28,22 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(362, 197);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "定时设置";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15F);
+            this.button1.Location = new System.Drawing.Point(441, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 115);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
+            this.button1.Text = "开始定时";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("宋体", 12F);
+            this.button2.Location = new System.Drawing.Point(441, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "分钟";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "秒钟";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(53, 93);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(86, 26);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(200, 93);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(86, 26);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "剩余时间";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 243);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(122, 28);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 306);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(637, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 399);
+            this.ClientSize = new System.Drawing.Size(671, 356);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "定时器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- UpperComputerDemo1/Form1.cs
+++ UpperComputerDemo1/Form1.cs
@@ -10,11 +10,125 @@ using System.Windows.Forms;
 
 namespace UpperComputerDemo1
 {
+    /// <summary>
+    /// 编程易学，思想难教
+    /// </summary>
     public partial class Form1 : Form
     {
+        UInt16 Timer_Value = 0;//定时值，分钟*60+秒钟 = Time_value
+        UInt16 Timer_Count = 0;//定时器计数
+        Byte Timer_Status = 0;//定时状态
+        // 0-停止状态 1-定时状态 2-暂停状态
+
         public Form1()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// 窗体初始化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int i;
+            for (i = 0; i < 60; i++)
+            {
+                comboBox1.Items.Add(i.ToString());
+                comboBox2.Items.Add(i.ToString());
+            }
+            comboBox1.Text = "0";
+            comboBox2.Text = "59";
+            button2.Enabled = false;
+            progressBar1.Value = 0;
+        }
+        /// <summary>
+        /// 定时器事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Timer_Count++;//每1秒 +1 
+            textBox1.Text = (Timer_Value - Timer_Count).ToString() + " 秒";//更新定时时间
+            progressBar1.Value = Timer_Count;//更新进度条
+            if (Timer_Count == Timer_Value)
+            {
+                timer1.Stop();
+                Timer_Count = 0;
+                Timer_Status = 0;//状态设置为0
+                button1.Text = "计时结束";
+                MessageBox.Show("倒计时结束", "Tips");
+                button1.Text = "开始计时";
+                textBox1.Text =string.Empty;
+                comboBox1.Enabled = true;
+                comboBox2 .Enabled = true;
+                Form1_Load(this, EventArgs.Empty);
+            }
+        }
+        /// <summary>
+        /// 停止事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Timer_Count = 0;
+            Timer_Status = 0;//状态设置为0
+            Form1_Load(sender, e);
+            button1.Text = "开始计时";
+            textBox1.Text = string.Empty;
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            Form1_Load(this, EventArgs.Empty);
+        }
+        /// <summary>
+        /// 开始计时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (Timer_Status)
+            {
+                // 0-停止状态 1-定时状态 2-暂停状态
+                case 0:
+                    Timer_Value = (ushort)((Convert.ToUInt16(comboBox1.Text,10) * 60)+Convert.ToUInt16(comboBox2.Text,10));//获取选框内的内容
+                    if (Timer_Value >0)
+                    {
+                        //开始计时
+                        textBox1.Text = Timer_Value.ToString()+" 秒";//文本框内显示倒计时数
+                        button1.Text = "暂停计时";
+                        button2.Enabled = true;
+                        comboBox1.Enabled = false;
+                        comboBox2.Enabled = false;
+                        Timer_Status = 1;//状态为1，定时状态
+                        //设置进度条
+                        progressBar1.Value = 0;//初始值设置为0
+                        progressBar1.Maximum = Timer_Value;//设置最大值为多少
+                        timer1.Start();//启动定时
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("定时时间不能为0", "TIps");
+                    }
+                    break;
+                case 1:
+                    timer1.Stop();
+                    button1.Text = "继续计时";
+                    Timer_Status = 2;//状态设置为2，
+                    break;
+                case 2:
+                    timer1.Start();
+                    button1.Text = "暂停计时";
+                    Timer_Status = 1;//计时状态
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
